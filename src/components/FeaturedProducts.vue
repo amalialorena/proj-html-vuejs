09195fedb0570940eb5d56bb25903f6bd8f77002
Amalia <amalialorenaferariu@gmail.com>
2021-12-14 10:50:24 +0100

    title component

--- src/components/FeaturedProducts.vue
+++ src/components/FeaturedProducts.vue
@@ -1,13 +1,28 @@
 <template>
     <section>
-
+        <Title 
+        :title="titleText"
+        :subtitle="subtitleText"/>
         
     </section>
 </template>
 
 <script>
+import Title from '../components/Title.vue'
 export default {
   name: 'FeaturedProducts',
+  components: {
+      Title,
+  },
+  props: {
+    
+  },
+  data () {
+      return {
+        titleText:"Featured Products",
+        subtitleText: "Must have products from our top sellers"
+      }
+  }
 }
 </script>
 
---
+++ src/components/Title.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="container">
+    <h1>{{title}}</h1>
+    <h4>{{subtitle}}</h4>
+</div>
+ 
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+      title: String,
+      subtitle: String,
+  }  
+}
+</script>
+
+<style scoped lang="scss">
+    .container {
+        text-align: center;
+        h1 {
+            margin-top: 80px;
+            margin-bottom: 8px;
+            font-size: 2.4em;
+            font-weight: 700;
+            position: relative;
+            display: inline-block;
+        }
+        h1::after, h1::before{
+            content: '';
+            width: 70%;
+            position: absolute;
+            border-bottom: 1px solid #c0bdbd;
+            top: 50%;
+            margin: 0 20px
+        }
+        h1::before {
+            left:-268px;
+        }
+        h4 {
+            font-size: .9em;
+            color: #585758;
+            margin-bottom: 40px;
+        }
+    }
+
+</style>
