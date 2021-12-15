de50d975841daddabb9107c45453c65a8581689e
Amalia <amalialorenaferariu@gmail.com>
2021-12-15 10:07:47 +0100

    quotes component

--- src/components/BestSeller.vue
+++ src/components/BestSeller.vue
@@ -5,25 +5,6 @@
             :subtitle="subtitleText"
         />
         <Carousel :small="smallCarousel"/>
-        <!-- <div class="carousel">
-            <div class="arrow">&#8249;</div>
-            <div class="wrapper">
-                <img src="../assets/img/spring_printed_dress-200x260.jpg" alt="">
-            </div>
-            <div class="wrapper">
-                <img src="../assets/img/modern_love_tee-200-260.jpg" alt="">
-            </div>
-            <div class="wrapper">
-                <img src="../assets/img/black_leather_jacket-200x260.jpg" alt="">
-            </div>
-            <div class="wrapper">
-                <img src="../assets/img/black_elegant_leather_jacket-200x260.jpg" alt="">
-            </div>
-             <div class="wrapper">
-                <img src="../assets/img/hipster_black_top-200x260.jpg" alt="">
-            </div>
-            <div class="arrow">&#8250;</div>
-        </div> -->
     </section>
 </template>
 
@@ -48,32 +29,6 @@ export default {
 
 
 <style scoped lang="scss">
-section {
-    // width: 70%;
-    // margin: auto;
-}
-//      .carousel {
-//         display: flex;
-//         align-items: center;
-//         margin-bottom: 60px;
-//         .wrapper {
-//             width: calc((100% / 4) - 40px);
-//             img {
-//                 width: 100%
-//             }
-//         }
-//         .arrow {
-//             height: 40px;
-//             width: 20px;
-//             text-align: center;
-//             line-height: 40px;
-//             background: grey;
-//             color: white;
-//             flex-shrink: 0;
-//         }
-//     }
 
-// }
-   
 
 </style>
--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,28 +1,60 @@
 <template>
   <section>
-      <div class="testimonial">
-          <img src="../assets/img/man_testimonial.png" alt="testimonial">
+    <div class="testimonial">
+      <div class="img-container">
+        <img src="../assets/img/man_testimonial.png" alt="testimonial" />
       </div>
       <div class="quote">
-          <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum vitae sit unde voluptatem aliquid commodi omnis, optio voluptatum. Cupiditate, consequatur!
-          </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum vitae
+          sit unde voluptatem aliquid commodi omnis, optio voluptatum.
+          Cupiditate, consequatur! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Vitae, doloremque.
+        </p>
+        <p class="author">
+           <strong>Dar&Iacute;o Pineda,</strong>  Theme fusion
+        </p>
+        <div class="dots"> &#9679; &#9675; </div>
       </div>
+    </div>
   </section>
 </template>
 
 <script>
-
 export default {
-  name: 'Quotes',
-}
+  name: "Quotes",
+};
 </script>
 
 
 <style scoped lang="scss">
-  section {
-      background-image: url("../assets/img/testimonials_home_1_bg.jpg");
-      height:50vh;
+section {
+    background-image: url("../assets/img/testimonials_home_1_bg.jpg");
+    height: 50vh;
+    display: flex;
+    justify-content: center;
+  .testimonial {
+        max-width: 700px;
+        margin: auto;
+        .img-container {
+            text-align: center;
+            height: 60px;
+            margin-bottom: 20px;
+            img {
+                height: 100%;
+            }
+        }
+        .quote {
+            text-align: center;
+            color: white;
+            font-style: italic;
+            font-weight: 500;
+            max-width: 700px;
+            .author {
+                font-style: normal;
+                margin: 10px 0;
+            }
+        }
   }
-
+}
 </style>
